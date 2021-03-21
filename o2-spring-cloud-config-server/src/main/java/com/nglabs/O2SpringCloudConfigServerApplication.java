547d4201fb8f145007cfcdfd0a873fb7fce54a18
marsjava <marikannan.cvp@gmail.com>
2021-03-21 18:00:41 +0530

    modify the limit-service properties

--- o2-spring-cloud-config-server/src/main/java/com/nglabs/O2SpringCloudConfigServerApplication.java
+++ o2-spring-cloud-config-server/src/main/java/com/nglabs/O2SpringCloudConfigServerApplication.java
@@ -2,7 +2,8 @@ package com.nglabs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
+@EnableConfigServer
 @SpringBootApplication
 public class O2SpringCloudConfigServerApplication {
 
