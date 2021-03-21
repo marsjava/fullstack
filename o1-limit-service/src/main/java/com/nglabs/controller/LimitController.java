59045417ad3c8a5d5f54767dcd84084365a0d268
marsjava <marikannan.cvp@gmail.com>
2021-03-21 16:40:37 +0530

    create config server service

--- o1-limit-service/src/main/java/com/nglabs/controller/LimitController.java
+++ o1-limit-service/src/main/java/com/nglabs/controller/LimitController.java
@@ -1,14 +1,21 @@
 package com.nglabs.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nglabs.config.LimitConfiguration;
 import com.nglabs.model.Limits;
 
 @RestController
 public class LimitController {
+	
+	@Autowired
+	private LimitConfiguration config;
+	
 	@GetMapping("/limits")
 	public Limits getLimits() {
-		return new Limits(1, 100);
+		return new Limits(config.getMinimum(), config.getMaximum());
+		//return new Limits(1, 100);
 	}
 }
