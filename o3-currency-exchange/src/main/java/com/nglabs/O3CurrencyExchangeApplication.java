62bb0d7b8e0bba5ae8fc6d1a639e40b5dd5699b2
marsjava <marikannan.cvp@gmail.com>
2021-03-22 14:03:03 +0530

    create currency exchange microservice

---
+++ o3-currency-exchange/src/main/java/com/nglabs/O3CurrencyExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.nglabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class O3CurrencyExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(O3CurrencyExchangeApplication.class, args);
+	}
+
+}
